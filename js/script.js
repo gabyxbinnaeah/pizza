a745c5ae9361f655ca78862b30ba4bb2a9b5f97c
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-04-24 22:17:33 +0300

    adds div to display total cost

---
+++ js/script.js
@@ -0,0 +1,49 @@
+//Business Logic
+function PizzaOrder(crust,topping) {
+  this.crustCost = crust;
+  this.toppingCost = topping;
+}
+Pizza.prototype.order = function() {
+  return this.crustCost + this.toppingCost;
+}
+//User Interface Logic
+$(document).ready(function() {
+  $("#order").click(function(event) {
+    event.preventDefault();
+    let size = $("#size").val();
+    let crust = $("#crust").val();
+    let topping = $("#topping").val();
+    let toppingCost = 0;
+    let crustCost = 0;
+    if (crust == "Crispy") {
+      crustCost = crustCost + 200;
+    }
+    else if (crust == "Stuffed") {
+      crustCost =crustCost + 250;
+    }
+    else if (crust == "Gluten-free") {
+      crustCost =crustCost + 300;
+    }
+    if (topping == "Mushroom") {
+      toppingCost =toppingCost + 150;
+    }
+    else if (topping == "Sausage") {
+      toppingCost =toppingCost + 200;
+    }
+    else if (topping == "Green Pepper") {
+      toppingCost = toppingCost + 250;
+    }
+    if (size == "Small") {
+      toppingCost = toppingCost*1.5;
+    }
+    else if (size == "Medium") {
+      toppingCost = toppingCost*2;
+    }
+    else if (size == "Large" ) {
+      toppingCost = toppingCost*2.5
+    }
+    let newOrder = new Pizza(crustCost,toppingCost);
+   // $("#user").text("Account Name: " + userName);
+    $("#total").text(newOrder.order());
+  });
+});
