b4c58ff563c20654f46fbccd93245e664f6e3aab
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-04-25 00:15:06 +0300

    Adds constructor PizzaOrder in sript file

--- js/script.js
+++ js/script.js
@@ -3,47 +3,51 @@ function PizzaOrder(crust,topping) {
   this.crustCost = crust;
   this.toppingCost = topping;
 }
-Pizza.prototype.order = function() {
-  return this.crustCost + this.toppingCost;
-}
-//User Interface Logic
-$(document).ready(function() {
-  $("#order").click(function(event) {
-    event.preventDefault();
-    let size = $("#size").val();
-    let crust = $("#crust").val();
-    let topping = $("#topping").val();
-    let toppingCost = 0;
-    let crustCost = 0;
-    if (crust == "Crispy") {
-      crustCost = crustCost + 200;
-    }
-    else if (crust == "Stuffed") {
-      crustCost =crustCost + 250;
-    }
-    else if (crust == "Gluten-free") {
-      crustCost =crustCost + 300;
-    }
-    if (topping == "Mushroom") {
-      toppingCost =toppingCost + 150;
-    }
-    else if (topping == "Sausage") {
-      toppingCost =toppingCost + 200;
-    }
-    else if (topping == "Green Pepper") {
-      toppingCost = toppingCost + 250;
-    }
-    if (size == "Small") {
-      toppingCost = toppingCost*1.5;
-    }
-    else if (size == "Medium") {
-      toppingCost = toppingCost*2;
-    }
-    else if (size == "Large" ) {
-      toppingCost = toppingCost*2.5
-    }
-    let newOrder = new Pizza(crustCost,toppingCost);
-   // $("#user").text("Account Name: " + userName);
-    $("#total").text(newOrder.order());
-  });
-});
+// PizzaOrder.prototype.order = function(){
+//   return this.crustCost + this.toppingCost;
+// }
+
+
+
+// //User Interface Logic
+// $(document).ready(function() {
+//   $("form#showOrder1").submit(function(event) {
+//     event.preventDefault();
+//     let size = $("#size").val();
+//     let crust = $("#crust").val();
+//     let topping = $("#toppings").val();
+//     let toppingCost = 0;
+//     let crustCost = 0;
+//     if (crust ==="1") {
+//       crustCost +=250;
+//     }
+//     else if (crust ==="2") {
+//       crustCost+=300;
+//     }
+//     else if (crust ==="3") {
+//       crustCost+=350;
+//     }
+//     if (topping === "1") {
+//       toppingCost+=100;
+//     }
+//     else if (topping === "2") {
+//       toppingCost+= 200;
+//     }
+//     else if (topping === "3") {
+//       toppingCost += 300;
+//     }
+//     if (size === "1") {
+//       toppingCost = toppingCost*1.5;
+//     }
+//     else if (size === "2") {
+//       toppingCost = toppingCost*2.5;
+//     }
+//     else if (size === "3" ) {
+//       toppingCost = toppingCost*3.5;
+//     }
+
+//     let newOrder = new PizzaOrder(crustCost,toppingCost);
+//    // $("#user").text("Account Name: " + userName);
+//     $("#totalBalance").text(newOrder.order());
+//   });
+// });
